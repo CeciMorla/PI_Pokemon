5bc4930766e4a6966f1849ae737b948a443a832a
Cecilia Morlacchi <90002130+CeciMorla@users.noreply.github.com>
2022-01-11 21:47:18 -0300

    update

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import style from './Landing.module.css';
+import img from './img/Bienvenidos.png'
 
 const Landing = ()=>{
     return (
         <div className={style.landing}>
-            <h1>Bienvenido al mundo Pokemon</h1>
+            <img src={img} className={style.img}/>
             <Link to='/home'>
-                <button>Entrar</button>
+                <button className={style.btn}>Entrar</button>
             </Link>
         </div>
     );
