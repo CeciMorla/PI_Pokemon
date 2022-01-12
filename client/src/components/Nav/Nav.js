2560ec71bfa419e130e6425fa6d7c7fefd01fd40
Cecilia Morlacchi <90002130+CeciMorla@users.noreply.github.com>
2022-01-12 19:16:57 -0300

    update

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar.js";
+import style from './Nav.module.css';
+import img from './img/5a0596df9cf05203c4b60445.png';
 
 
 const Nav = ({types,handleFilterType,handleFilterCreated,handleOrderSort}) =>{
     return (
-        <div>
+        <div className={style.container}>
+            <div className={style.divImg}>
+            <img src={img} className={style.img}/>
+            </div>
+            <div className={style.divCreate}>
             <Link to='/create'>
-                <button>Crear Pokemon</button>
+                <button className={style.create}>Crear Pokemon</button>
             </Link>
-            <select onChange={(e)=> handleOrderSort(e)}>
+            </div>
+            <div className={style.divSelectors}>
+            <select onChange={(e)=> handleOrderSort(e)} className={style.selectOne}>
                 <option value='asc'>A-Z</option>
                 <option value='des'>Z-A</option>
                 <option value='attack+'>Mayor Fuerza</option>
@@ -20,7 +28,7 @@ const Nav = ({types,handleFilterType,handleFilterCreated,handleOrderSort}) =>{
                 <option value='created'>Creados</option>
                 <option value='exist'>Existentes</option>
             </select>
-            <select onChange={(e)=> handleFilterType(e)}>
+            <select onChange={(e)=> handleFilterType(e)} className={style.selectTree}>
                 <option value='allTypes'>Tipos</option>
                 {
                     types?.map(t=>{
@@ -33,7 +41,10 @@ const Nav = ({types,handleFilterType,handleFilterCreated,handleOrderSort}) =>{
                 }
                     
             </select>
+            </div>
+            <div className={style.searchBar}>
             <SearchBar/>
+            </div>
         </div>
     );
 }
