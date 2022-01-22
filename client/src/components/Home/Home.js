468bfa84184843a6733433af6e80e07fe1a0f724
Cecilia Morlacchi <90002130+CeciMorla@users.noreply.github.com>
2022-01-22 15:30:57 -0300

    Fix Home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -16,7 +16,7 @@ const Home = () =>{
     const types = useSelector((state) => state.types);
     //const pokemons = useSelector((state)=> state.pokemons);
     const state = useSelector((state) => state)
-    const [order,setOrder] = useState('');
+    //const [,setOrder] = useState('');
     const [currentPage,setCurrentPage] = useState(1);
     const pokemonPage = 12;
     const lastPokemon = currentPage * pokemonPage; // 1 * 12 = 12
@@ -49,7 +49,7 @@ const Home = () =>{
         e.preventDefault();
         dispatch(orderBy(e.target.value));
         setCurrentPage(1);
-        setOrder(e.target.value);
+        //setOrder(e.target.value);
     }
 
     
